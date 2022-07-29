26f61d0d6f0fe201b83bb4611012ccf78334fc2c
Benjamín Muľár <mularbenjamin@gmail.com>
2022-07-29 15:32:04 +0200

    Add function for playing a round
    Added function that will take player's & computer's selection as
    parameters and returns the outcome based on RPS rules, tested it with
    console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,22 @@ function getComputerChoice() {
     const choice = choices[randomNum];
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelection === computerSelection) {
+        return `It's a tie! ${playerSelection} ties with ${computerSelection}`;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beats Paper";
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
